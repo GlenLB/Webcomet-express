da8bdc580de9cfe8a6d129ae32ca9c2a827f5a76
Glen Le Baill <lebaill.glen@gmail.com>
2019-05-27 11:06:04 +0200

    Améliorations title et meta description

--- routes/index.js
+++ routes/index.js
@@ -31,7 +31,7 @@ function creerUrlCanonique(req, res) {
 router.get('/', function (req, res, next) {
 	let p = new Page(
 		"WebComet.fr &#x2022; Webmaster à Rennes &#x2022; Création de site internet et SEO",
-		"Vous cherchez un webmaster ou un développeur web freelance à Rennes ou ailleurs pour la création de votre site internet ? Vous cherchez un consultant SEO pour améliorer votre référencement naturel ? Alors contactez-moi pour obtenir un site web optimisé pour le SEO.",
+		"Webcomet.fr, Webmaster à Rennes et Consultant SEO. Contactez-moi pour créer un site internet ou pour améliorer votre référencement sur Google :-)",
 		creerUrlCanonique(req, res),
 		"Création de site internet et optimisation SEO<br>Webmaster à Rennes"
 	)
@@ -41,8 +41,8 @@ router.get('/', function (req, res, next) {
 /* Page création de sites internet */
 router.get("/creation-site-internet", function (req, res, next) {
 	let p = new Page(
-		"Création de site internet à Rennes et SEO &#x2022; Webmaster à Rennes &#x2022; WebComet.fr",
-		"Vous avez besoin de faire réaliser la création d'un site internet pour présenter votre activité ? Webmaster basé à Rennes, je peux vous créer un site internet correspondant à vos besoins quelle que soit votre localisation dans le monde.",
+		"Création de site internet à Rennes et SEO &#x2022; Webmaster à Rennes",
+		"Webmaster basé à Rennes, je suis spécialisé en création de site internet optimisé pour le SEO. Soyez visible sur Google pour capter votre clientèle cible.",
 		creerUrlCanonique(req, res),
 		"Création de site internet à Rennes<br>Webmaster basé à Rennes"
 	)
@@ -52,8 +52,8 @@ router.get("/creation-site-internet", function (req, res, next) {
 /* Page consultant SEO */
 router.get("/consultant-seo", function (req, res, next) {
 	let p = new Page(
-		"Consultant SEO à Rennes &#x2022; Consultant référencement naturel à Rennes &#x2022; WebComet.fr",
-		"Consultant SEO indépendant à Rennes, je dispose des compétences pour améliorer la visibilité de votre site web sur les moteurs de recherche comme Google. En améliorant le référencement naturel de votre site internet, vous capterez plus de trafic qualifié sur votre site web.",
+		"Consultant SEO à Rennes &#x2022; Améliorer référencement naturel Google",
+		"Consultant SEO indépendant à Rennes, je dispose des compétences pour améliorer la visibilité de votre site web sur les moteurs de recherche comme Google.",
 		creerUrlCanonique(req, res),
 		"Consultant SEO à Rennes<br>Optimisations en référencement naturel"
 	)
